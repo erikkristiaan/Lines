97a518a405145ac4a65ba5b581c74c3cfdb6dced
Erik <allardae@gmail.com>
2022-05-17 17:29:24 +0200

    added include guard

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -1,6 +1,5 @@
-#include <opencv2/opencv.hpp>
+#pragma once
 
-#include <iostream>
-
+#include <opencv2/imgcodecs.hpp>
 
 std::vector<std::array<uchar, 3>> remove_duplicates(std::vector<std::array<uchar, 3>> &vec);
